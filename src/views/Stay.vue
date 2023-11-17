20c9a46e2881c20420a0d965eb15c0f6a28f1040
semajame <jamesanquillano@gmail.com>
2023-11-17 10:38:14 +0800

    update

--- src/views/Stay.vue
+++ src/views/Stay.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>STAY</h1>
+  <div class="home__overlay"></div>
+  <main class="stay__hero">
+    <div class="hero__content">
+      <span> {{ stayWelcome }} </span>
+
+      <h1>{{ stayHeader }}</h1>
+      <button class="get__to__know" @click="toggleKnow">
+        {{ stayButton }}
+      </button>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      stayWelcome: "Stay with us",
+      stayHeader: "Attention to detail like never before.",
+      stayButton: "Explore our Hotel",
+    };
+  },
+
+  methods: {
+    toggleKnow() {
+      const section = document.getElementById("about__section");
+
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
