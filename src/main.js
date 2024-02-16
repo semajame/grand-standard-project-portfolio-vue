d91c90d1ab9575a434283b55eb445e69eec12543
semajame <jamesanquillano@gmail.com>
2024-02-16 09:15:25 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import style from "./styles/style.css";
-import reset from "./styles/reset.css";
-import header from "./styles/header.css";
-import about from "./styles/about.css";
-import contact from "./styles/contact.css";
-import stay from "./styles/stay.css";
+import style from "./styles/style.css?module";
+import reset from "./styles/reset.css?module";
+import header from "./styles/header.css?module";
+import about from "./styles/about.css?module";
+import contact from "./styles/contact.css?module";
+import stay from "./styles/stay.css?module";
 
 const app = createApp(App);
 
